a7474bfc8514acfb0b1842512d3c2e40cab64139
vuraweg <virat52418@gmail.com>
2025-10-06 13:12:32 +0530

    Updated GuidedResumeBuilder.tsx

--- src/components/GuidedResumeBuilder.tsx
+++ src/components/GuidedResumeBuilder.tsx
@@ -2839,6 +2839,7 @@ const handleGenerateProjectBullets = async (
                     resumeData={optimizedResume}
                     userType={userType}
                     showControls={true}
+                    defaultZoom={0.65}
                   />
                 ) : (
                   <div className="flex items-center justify-center h-full text-center px-6 py-12 text-gray-500 dark:text-gray-400">
